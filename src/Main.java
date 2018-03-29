8be7987a74404784f7e0be1a8fb8269c3187d18c
Kurniawan Yudha Putrama <37765967+kurniawan-6706161060@users.noreply.github.com>
2018-03-29 08:58:09 +0700

    Add project file
    Add file
    - Main.java
    - faktorial.iml

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+/**
+ * Create by Kurniawan Yudha Putrama
+ * Telkom Univwersity
+ * D3IF-40-04
+ */
+public class Main {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Masukkan angka : ");
+        int n = in.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            // Space
+            for (int y = 0; y < n-(i+1); y++) {
+                System.out.print(" ");
+            }
+            // Star
+            for (int y = 0; y < (i+1)*2; y++) {
+                System.out.print("*");
+            }
+            // End Line
+            System.out.println();
+        }
+        for (int i = n-1; i >= 0; i--) {
+            // Space
+            for (int y = 0; y < n-(i+1); y++) {
+                System.out.print(" ");
+            }
+            // Star
+            for (int y = 0; y < (i+1)*2; y++) {
+                System.out.print("*");
+            }
+            // End Line
+            System.out.println();
+        }
+    }
+}
